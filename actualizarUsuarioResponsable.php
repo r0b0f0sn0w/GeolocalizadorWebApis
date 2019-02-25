025197a912af14aa59ae672cc0b776e5c8c5606c
r0b0f0sn0w <r0b0f0sn0w@users.noreply.github.com>
2019-02-25 17:45:56 -0500

    cosa

--- actualizarUsuarioResponsable.php
+++ actualizarUsuarioResponsable.php
@@ -1,7 +1,7 @@
 <?php
 include ("DAL/conexion.php");
-
 $json=file_get_contents('php://input');
+
 $obj=json_decode($json);
 $id_usuarioResp =filter_var($obj->id_usuarioResp);
 $nombre =filter_var($obj->nombre);
@@ -10,12 +10,11 @@ $apemat =filter_var($obj->apemat);
 $telefono =filter_var($obj->telefono);
 $telefono2 =filter_var($obj->telefono2);
 $direccion =filter_var($obj->direccion);
-$correo_electronico =filter_var($obj->correo_electronico);
-$estado = 0;
-$sql="call SP_ACTUALIZAR_USUARIO_RESPONSABLE('$id_usuarioResp','$nombre','$apepat','$apemat','$telefono','$telefono2','$direccion');";
 
+$sql="call SP_ACTUALIZAR_USUARIO_RESPONSABLE('$id_usuarioResp','$nombre','$apepat','$apemat','$telefono','$telefono2','$direccion');";
+echo $sql;
 if(ejecutar($sql)){
-    echo "update";
+    echo "update";      
 }else{
     echo "no :c";
 }
