2e851f36085adddf10396c4b6eaf3afa49dcd39a
r0b0f0sn0w <r0b0f0sn0w@users.noreply.github.com>
2019-02-25 17:14:26 -0500

    Primera version de WebServices

---
+++ actualizarUsuarioResponsable.php
@@ -0,0 +1,22 @@
+<?php
+include ("DAL/conexion.php");
+
+$json=file_get_contents('php://input');
+$obj=json_decode($json);
+$id_usuarioResp =filter_var($obj->id_usuarioResp);
+$nombre =filter_var($obj->nombre);
+$apepat =filter_var($obj->apepat);
+$apemat =filter_var($obj->apemat);
+$telefono =filter_var($obj->telefono);
+$telefono2 =filter_var($obj->telefono2);
+$direccion =filter_var($obj->direccion);
+$correo_electronico =filter_var($obj->correo_electronico);
+$estado = 0;
+$sql="call SP_ACTUALIZAR_USUARIO_RESPONSABLE('$id_usuarioResp','$nombre','$apepat','$apemat','$telefono','$telefono2','$direccion');";
+
+if(ejecutar($sql)){
+    echo "update";
+}else{
+    echo "no :c";
+}
+?>
---
+++ crearUsuarioResponsable.php
@@ -0,0 +1,22 @@
+<?php
+include ("DAL/conexion.php");
+
+$json=file_get_contents('php://input');
+$obj=json_decode($json);
+$nombre =filter_var($obj->nombre);
+$apepat =filter_var($obj->apepat);
+$apemat =filter_var($obj->apemat);
+$telefono =filter_var($obj->telefono);
+$telefono2 =filter_var($obj->telefono2);
+$direccion =filter_var($obj->direccion);
+$correo_electronico =filter_var($obj->correo_electronico);
+$password =filter_var($obj->password);
+$estado = 0;
+$sql="call SP_REGISTRAR_USUARIO_RESPONSABLE('$nombre','$apepat','$apemat','$telefono','$telefono2','$direccion','$correo_electronico','$password','$estado');";
+
+if(ejecutar($sql)){
+    echo "ejecutado correcto";
+}else{
+    echo "no :c";
+}
+?>
