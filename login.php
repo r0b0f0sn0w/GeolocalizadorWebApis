4139fce58bd1287b1aa93b59e383175f52127dbb
r0b0f0sn0w <r0b0f0sn0w@users.noreply.github.com>
2019-03-24 19:18:31 -0400

    asd

--- login.php
+++ login.php
@@ -1,7 +1,6 @@
 <?php
 include_once 'DAL/conexion.php';
 
-$respuesta = array();
 $respuesta["usuario"] = array();  
 
 $con = mysqli_connect("$host", "$username", "$password")or die("cannot connect server "); 
@@ -10,24 +9,19 @@ mysqli_select_db($con,"$db_name")or die("cannot select DB");
 $json    =  file_get_contents('php://input');
 $obj     =  json_decode($json);
 
-$QR =filter_var($obj->QR);
-$sql="call SP_LEER_QR('$QR');";
+$correo =filter_var($obj->correo);
+$jspassword =filter_var($obj->password);
+
+$sql="call SP_LOGIN('$correo','$jspassword');";
+echo $sql;
 $result=mysqli_query($con,$sql);
     while($row = mysqli_fetch_array($result)){
         // Array temporal para crear una sola categoría
         $tmp = array();
-        $tmp["ur_nombre"] = $row["ur_nombre"];
-        $tmp["ur_apellidoPat"] = $row["ur_apellidoPat"];
-        $tmp["ur_correo_electronico"] = $row["ur_correo_electronico"];
-        $tmp["up_nombre"] = $row["up_nombre"];
-        $tmp["up_apellidoPat"] = $row["up_apellidoPat"];
-        $tmp["up_apellidoMat"] = $row["up_apellidoMat"];
-        $tmp["up_telefono"] = $row["up_telefono"];
-        $tmp["up_telefono2"] = $row["up_telefono2"];
-        $tmp["up_direccion"] = $row["up_direccion"];
-        $tmp["up_padecimiento"] = $row["up_padecimiento"];
-        $tmp["up_descripcion"] = $row["up_descripcion"];
-        
+        $tmp["id_usuarioResp"] = $row["id_usuarioResp"];
+        $tmp["nombre"] = $row["nombre"];
+        $tmp["apellidoPat"] = $row["apellidoPat"];
+        $tmp["estado"] = $row["estado"];
         // Push categoría a final json array
         array_push($respuesta["usuario"], $tmp);
     }
