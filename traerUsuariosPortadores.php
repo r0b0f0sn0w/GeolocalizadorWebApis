a247cd9a377c7d3d4f378ee7b827eb4a9880d496
r0b0f0sn0w <r0b0f0sn0w@users.noreply.github.com>
2019-03-27 12:32:40 -0400

    Listar usuarios

--- traerUsuariosPortadores.php
+++ traerUsuariosPortadores.php
@@ -9,18 +9,19 @@ mysqli_select_db($con,"$db_name")or die("cannot select DB");
 $json    =  file_get_contents('php://input');
 $obj     =  json_decode($json);
 
-$correo =filter_var($obj->correo);
-$jspassword =filter_var($obj->password);
+$id_usuario =filter_var($obj->usuario);
 
-$sql="call SP_LOGIN('$correo','$jspassword');";
+$sql="call SP_LEER_USUARIOSPORTADORES('$id_usuario');";
 $result=mysqli_query($con,$sql);
     while($row = mysqli_fetch_array($result)){
         // Array temporal para crear una sola categoría
         $tmp = array();
-        $tmp["id_usuarioResp"] = $row["id_usuarioResp"];
-        $tmp["nombre"] = $row["nombre"];
-        $tmp["apellidopat"] = $row["apellidopat"];
-        $tmp["estado"] = $row["estado"];
+        $tmp["up_nombre"] = $row["up_nombre"];
+        $tmp["ap_apellidoPat"] = $row["ap_apellidoPat"];
+        $tmp["ap_apellidoMat"] = $row["ap_apellidoMat"];
+        $tmp["up_telefono"] = $row["up_telefono"];
+        $tmp["up_telefono2"] = $row["up_telefono2"];
+        $tmp["up_direccion"] = $row["up_direccion"];
         // Push categoría a final json array
         array_push($respuesta["usuario"], $tmp);
     }
